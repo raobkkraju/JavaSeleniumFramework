16b53daf53a0988da4c96806942d51aaad444d1c
raobkkraju <raobkkraju@gmail.com>
2022-11-08 13:56:01 +0530

    Commit"GitPractice-3");

--- April2022_8AM_seleniumBatch/src/test/java/GitPractice/GitPractice.java
+++ April2022_8AM_seleniumBatch/src/test/java/GitPractice/GitPractice.java
@@ -6,6 +6,7 @@ public class GitPractice {
 		// TODO Auto-generated method stub
         System.out.println("GitPractice-1");
         System.out.println("GitPractice-2");
+        System.out.println("GitPractice-3");
 	}
 
 }
