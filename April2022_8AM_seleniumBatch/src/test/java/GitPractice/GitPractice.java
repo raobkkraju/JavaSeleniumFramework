9bf69b89f7338a725c681b65c5bd8314a2e3be69
raobkkraju <raobkkraju@gmail.com>
2022-11-08 10:52:01 +0530

    Commit-GitPractice-1 Added Package and Class

---
+++ April2022_8AM_seleniumBatch/src/test/java/GitPractice/GitPractice.java
@@ -0,0 +1,10 @@
+package GitPractice;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("GitPractice-1");
+	}
+
+}
