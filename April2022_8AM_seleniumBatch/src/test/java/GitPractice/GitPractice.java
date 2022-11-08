d96350325ca7e8b142d48edb2edca1c376e1ce73
raobkkraju <raobkkraju@gmail.com>
2022-11-08 13:55:19 +0530

    CommitMessage("GitPractice-2");

--- April2022_8AM_seleniumBatch/src/test/java/GitPractice/GitPractice.java
+++ April2022_8AM_seleniumBatch/src/test/java/GitPractice/GitPractice.java
@@ -5,6 +5,7 @@ public class GitPractice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("GitPractice-1");
+        System.out.println("GitPractice-2");
 	}
 
 }
